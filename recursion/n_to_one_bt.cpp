9b14ca35a243f86dfebcccac50c2b605eb06fb06
jaswanthinvalley <jaswanthvenkat001@gmail.com>
2026-01-09 13:52:05 +0530

    recursion problem

---
+++ recursion/n_to_one_bt.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void f(int i, int n) {      
+    if(i > n){return;}      
+    f(i + 1,n); 
+    cout<<i<<"\n";           
+}                           
+
+int main() {
+    int n;
+    cin>>n;
+    f(1,n);
+
+
+}
