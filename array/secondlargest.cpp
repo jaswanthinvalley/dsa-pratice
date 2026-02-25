1390f3cf6ff20df71864c8f852175f9cb99a114f
jaswanthinvalley <jaswanthvenkat001@gmail.com>
2026-02-25 22:38:38 +0530

    sorting pratice

---
+++ array/secondlargest.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+   vector <int> v(5);
+
+    cin>>v[0] >> v[1] >> v[2] >> v[3];
+    int n = v.size();
+    int largest = v[n-1];
+    for(int i=n-2; i<=0;i--){
+        if(v[i] )
+    }
+
+}
